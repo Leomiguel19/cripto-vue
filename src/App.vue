0fac19ad91c8828072a1ff7d887986dd7b22e8d4
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-25 23:31:53 -0400

    Crear componente de Cotizacion

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   import Alerta from './components/Alerta.vue'
   import Spinner from './components/Spinner.vue'
   import useCripto from './composables/useCripto'
+  import Cotizacion from './components/Cotizacion.vue'
 
   const { monedas, criptomonedas, cotizacion, cargando, obtenerCotizacion, mostrarCotizacion} = useCripto();
 
@@ -72,30 +73,11 @@
         <input type="submit" value="Cotizar">
       </form>
 
-      <Spinner 
-        v-if="cargando"
-      />
-
-      <div 
-        class="contenedor-resultado"
+      <Spinner v-if="cargando"/>
+      <Cotizacion
         v-if="mostrarCotizacion"
-      >
-        <h2>Cotizacion</h2>
-
-        <div class="resultado">
-          <img 
-            :src="'Https://cryptocompare.com'+cotizacion.IMAGEURL" 
-            alt="imagen cripto"
-          >
-          <div>
-            <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
-            <p>El precio mas alto del dia: <span>{{ cotizacion.HIGHDAY }}</span></p>
-            <p>El precio mas bajo del dia: <span>{{ cotizacion.LOWDAY }}</span></p>
-            <p>Variacion ultimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span></p>
-            <p>Ultima Actualizacion: <span>{{ cotizacion.LASTUPDATE }}</span></p>
-          </div>
-        </div>
-      </div>
+        :cotizacion="cotizacion"
+      />
     </div>
   </div>
 </template>
