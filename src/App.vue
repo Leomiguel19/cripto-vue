80f225781de9b49c108f8b75e21a5fb66c9a9879
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-25 21:59:11 -0400

    Mostrar el resultado de la cotizacion

--- src/App.vue
+++ src/App.vue
@@ -93,6 +93,24 @@
 
         <input type="submit" value="Cotizar">
       </form>
+
+      <div class="contenedor-resultado">
+        <h2>Cotizacion</h2>
+
+        <div class="resultado">
+          <img 
+            :src="'Https://cryptocompare.com'+cotizacion.IMAGEURL" 
+            alt="imagen cripto"
+          >
+          <div>
+            <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
+            <p>El precio mas alto del dia: <span>{{ cotizacion.HIGHDAY }}</span></p>
+            <p>El precio mas bajo del dia: <span>{{ cotizacion.LOWDAY }}</span></p>
+            <p>Variacion ultimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span></p>
+            <p>Ultima Actualizacion: <span>{{ cotizacion.LASTUPDATE }}</span></p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
